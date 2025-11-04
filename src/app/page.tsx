a54e8024713ba549987fcd22b07b1f4621d1e817
Alejandro Camacho Olivares <alejandro@uzu.digital>
2025-11-04 17:51:50 -0600

    feat: charts dashboard started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,30 @@ import {
 import { Separator } from "@/components/ui/separator"
 import { AppSidebar } from "@/components/app-sidebar"
 import { currentUser } from "@clerk/nextjs/server";
+import { Bar, BarChart } from "recharts"
+import { ChartConfig, ChartContainer } from "@/components/ui/chart"
 
 export default async function Home() {
   const user = await currentUser()
+
+  const chartData = [
+    { month: "January", desktop: 186, mobile: 80 },
+    { month: "February", desktop: 305, mobile: 200 },
+    { month: "March", desktop: 237, mobile: 120 },
+    { month: "April", desktop: 73, mobile: 190 },
+    { month: "May", desktop: 209, mobile: 130 },
+    { month: "June", desktop: 214, mobile: 140 },
+  ]
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "#2563eb",
+    },
+    mobile: {
+      label: "Mobile",
+      color: "#60a5fa",
+    },
+  } satisfies ChartConfig
 
   return (
     <SidebarProvider>
@@ -27,9 +48,17 @@ export default async function Home() {
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="bg-muted/50 aspect-video rounded-xl">
+              <span className="">Bienvenid@,</span>
               Bienvenid@, {" "} {user?.firstName} {" "} {user?.lastName}
             </div>
-            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl">
+              <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+                <BarChart accessibilityLayer data={chartData}>
+                  <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+                  <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+                </BarChart>
+              </ChartContainer>
+            </div>
             <div className="bg-muted/50 aspect-video rounded-xl" />
           </div>
           <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
