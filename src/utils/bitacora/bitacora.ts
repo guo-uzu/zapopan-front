09753f01c0afedae461bda0037edea33dd334a5b
Alejandro Camacho Olivares <alejandro@uzu.digital>
2026-04-13 17:38:44 -0600

    feat: search owner of post N/A added

--- src/utils/bitacora/bitacora.ts
+++ src/utils/bitacora/bitacora.ts
@@ -2,11 +2,11 @@ import { Table } from "@tanstack/react-table";
 import { BitacoraTable } from "@/types/bitacoraTable";
 
 export const getValueFilter = (
-    filterName: string,
-    table: Table<BitacoraTable>,
+  filterName: string,
+  table: Table<BitacoraTable>,
 ) => {
-    return (
-        (table.getState().columnFilters.find((f) => f.id === filterName)
-            ?.value as string) ?? ""
-    );
+  return (
+    (table.getState().columnFilters.find((f) => f.id === filterName)
+      ?.value as string) ?? ""
+  );
 };
