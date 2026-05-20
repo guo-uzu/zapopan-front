aebd64a2b49d78ec5b16927ac6793a5449a2c310
Alejandro Camacho <alejandro@uzu.digital>
2026-05-20 17:56:58 -0600

    fix: fixed selected areas in respuestas page.tsx

--- src/types/respuestas.ts
+++ src/types/respuestas.ts
@@ -1,46 +1,47 @@
 interface Area {
-    id: string
-    color: string
-    label: string
-    value: string
+  id: string
+  color: string
+  label: string
+  value: string
+  [key: string]: string
 }
 
 interface User {
-    avatar_url?: string
-    email?: string
-    full_name: string
+  avatar_url?: string
+  email?: string
+  full_name: string
 }
 
 interface UpdatedUser {
-    full_name: string
+  full_name: string
 }
 
 interface ResponseFromAPI {
-    id: string
-    created_at: string
-    description_gob: string
-    description_jjf: string
-    labels_areas: Area[]
-    latest_updated_user_id: UpdatedUser[] | UpdatedUser | null
-    title: string
-    updated_at: string
-    user_id: User[] | User
+  id: string
+  created_at: string
+  description_gob: string
+  description_jjf: string
+  labels_areas: Area[]
+  latest_updated_user_id: UpdatedUser[] | UpdatedUser | null
+  title: string
+  updated_at: string
+  user_id: User[] | User
 }
 
 interface DefaultForm {
-    id?: string
-    title: string
-    jjfDescription: string
-    gobDescription: string
-    selectedAreas: Area[]
+  id?: string
+  title: string
+  jjfDescription: string
+  gobDescription: string
+  selectedAreas: Area[]
 }
 
 interface FormData {
-    id?: string
-    title?: string;
-    jjfDescription?: string;
-    gobDescription?: string;
-    selectedAreas?: Area[];
+  id?: string
+  title?: string;
+  jjfDescription?: string;
+  gobDescription?: string;
+  selectedAreas?: Area[];
 }
 
 export type { ResponseFromAPI, DefaultForm, FormData, Area, User }
