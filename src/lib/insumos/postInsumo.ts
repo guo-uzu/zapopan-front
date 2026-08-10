1c9bebeeddcdd24f0697420dc0472970112fbf83
Alejandro Camacho <alejandro@uzu.digital>
2026-08-10 17:57:26 -0600

    feat: added test in server synology with cloudflare

--- src/lib/insumos/postInsumo.ts
+++ src/lib/insumos/postInsumo.ts
@@ -1,5 +1,6 @@
 export const postInsumos = async (file: File) => {
-  const URL = "http://localhost:8000/api/insumos/upload-file";
+  // const URL = "http://localhost:8000/api/insumos/upload-file";
+  const URL = "http://zapopan-api.insumos.appsuzu.fun/api/insumos/upload-file";
   const formData = new FormData();
   formData.append("file", file);
   const data = await fetch(URL, {
